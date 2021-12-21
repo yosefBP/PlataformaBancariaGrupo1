c9b9243f29e8168067ada118ad49392ed81c978f
yosefBP <juan_karlos_999@hotmail.com>
2021-12-20 22:02:26 -0500

    app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import team from './dt.jpg';
 import './App.css';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <div><img src={team} alt="team" /></div>
     </div>
   );
 }
